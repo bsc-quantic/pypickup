f339ef071dc279f3df6b1e71f1b1ce620a0672b5
Germán Navarro <ge.najim@gmail.com>
2022-08-16 17:47:21 +0200

    Fixed the cmd.add main argument again.

--- pypi_cache/cmd/add.py
+++ pypi_cache/cmd/add.py
@@ -7,7 +7,7 @@ class Add:
     @staticmethod
     def init_subparser(parser: argparse.ArgumentParser):
         parser.add_argument("packageName", type=str, default="", help="Python package to download.")
-        parser.add_argument("-p", "--pypi-local-path", dest="pypiLocalPath", type=str, default="~/.pypi-cache/", help="Local root path to download the package from PyPI.")
+        parser.add_argument("-p", "--index-path", dest="pypiLocalPath", type=str, default="~/.pypi-cache/", help="Local root path in which the package from the PyPI repository will be downloaded.")
 
     @staticmethod
     def run(args: argparse.Namespace):
