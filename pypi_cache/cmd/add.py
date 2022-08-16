4de6d0264bd3acb9f79dbab558403ba62e6ac3b9
Germán Navarro <ge.najim@gmail.com>
2022-08-16 16:57:46 +0200

    Updated cmd/add.py class to properly works as a main function.

--- pypi_cache/cmd/add.py
+++ pypi_cache/cmd/add.py
@@ -1,11 +1,20 @@
 import argparse
 
+from pypi_cache.pypiCacheManager import LocalPyPIController
 
 class Add:
     @staticmethod
     def init_subparser(parser: argparse.ArgumentParser):
-        parser.add_argument("pkg", help="Package(s) to track", action="store")
+        parser.add_argument("packageName", type=str, default="", help="Python package to download.")
+        parser.add_argument("-p", "--pypiLocalPath", dest="pypiLocalPath", type=str, default="./localPyPI/", help="Local root path to download the package from PyPI.")
 
     @staticmethod
     def run(args: argparse.Namespace):
-        pass
+        controllerInstance = LocalPyPIController()
+        
+        controllerInstance.parseScriptArguments(args)
+        needToDownloadFiles: bool = controllerInstance.initLocalRepo()
+        if needToDownloadFiles:
+            controllerInstance.downloadFiles()
+        else:
+            print("Package " + controllerInstance.packageName + " is being already tracked. Try to update it instead to synchronize changes.")
