17b7161fcdfe8721377ba17c33300578fd7a60f5
Germán Navarro <ge.najim@gmail.com>
2022-08-17 13:50:35 +0200

    Fixed a bug in the definition of the ~ path in add command main method.

--- pypi_cache/cmd/add.py
+++ pypi_cache/cmd/add.py
@@ -1,4 +1,5 @@
 import argparse
+from pathlib import Path
 
 from pypi_cache.pypiCacheManager import LocalPyPIController
 
@@ -6,8 +7,8 @@ from pypi_cache.pypiCacheManager import LocalPyPIController
 class Add:
     @staticmethod
     def init_subparser(parser: argparse.ArgumentParser):
-        parser.add_argument("packageName", type=str, default="", help="Python package to download.")
-        parser.add_argument("-p", "--index-path", dest="pypiLocalPath", type=str, default="~/.pypi-cache/", help="Local root path in which the package from the PyPI repository will be downloaded.")
+        parser.add_argument("packageName", type=str, default="", help="Python package to add to the local repository.")
+        parser.add_argument("-p", "--index-path", dest="pypiLocalPath", type=str, default=str(Path.home()) + "/.pypi-cache/", help="Local root path in which the package from the PyPI repository will be synchronized.")
 
     @staticmethod
     def run(args: argparse.Namespace):
