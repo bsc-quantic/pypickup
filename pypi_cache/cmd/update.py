e93ac593f21b4e225cbe6e377b1dbc0819c7cfef
Germán Navarro <ge.najim@gmail.com>
2022-08-17 14:11:54 +0200

    Defined the 'update' command skeleton.

--- pypi_cache/cmd/update.py
+++ pypi_cache/cmd/update.py
@@ -1,11 +1,22 @@
 import argparse
+from pathlib import Path
 
+from pypi_cache.pypiCacheManager import LocalPyPIController
 
 class Update:
     @staticmethod
     def init_subparser(parser: argparse.ArgumentParser):
-        pass
+        parser.add_argument("packageName", type=str, default="", help="Python package to add to the local repository.")
+        parser.add_argument("-p", "--index-path", dest="pypiLocalPath", type=str, default=str(Path.home()) + "/.pypi-cache/", help="Local root path in which the package from the PyPI repository will be synchronized.")
 
     @staticmethod
     def run(args: argparse.Namespace):
-        pass
+        controllerInstance = LocalPyPIController()
+
+        controllerInstance.parseScriptArguments(args)
+        canSynchronize: bool = controllerInstance.isAlreadyAdded()
+        if canSynchronize:
+            syncMessage = controllerInstance.synchronizeWithRemote()
+            print(syncMessage)
+        else:
+            print("Package " + controllerInstance.packageName + " has not been added to the local repository yet. Run the 'add' command first.")
