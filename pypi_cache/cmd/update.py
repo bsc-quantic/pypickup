3379b0829032521d0f9809ba51830a99e8a48369
Germán Navarro <ge.najim@gmail.com>
2022-09-12 16:23:33 +0200

    Fixed the -p param default path to the current wd.

--- pypi_cache/cmd/update.py
+++ pypi_cache/cmd/update.py
@@ -9,7 +9,7 @@ class Update:
     def init_subparser(parser: argparse.ArgumentParser):
         parser.add_argument("packageName", type=str, default="", help="Python package to add to the local repository.")
         parser.add_argument("-s", "--only-src", dest="onlySources", default=False, action="store_true", help="Download only the source files (.zip and .tar.gz). Disabled by default.")
-        parser.add_argument("-p", "--index-path", dest="pypiLocalPath", type=str, default=os.getenv('PYPICACHE_INDEX_PATH', default="."), help="Local root path in which the package from the PyPI repository will be synchronized.")
+        parser.add_argument("-p", "--index-path", dest="pypiLocalPath", type=str, default=os.getenv('PYPICACHE_INDEX_PATH', default="./.pypi-cache/"), help="Local root path in which the package from the PyPI repository will be synchronized.")
 
     @staticmethod
     def run(args: argparse.Namespace):
