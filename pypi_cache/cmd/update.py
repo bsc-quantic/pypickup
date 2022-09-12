f3137acc3e4f24a02b8052bc825a898d68cbf385
Germán Navarro <ge.najim@gmail.com>
2022-09-12 13:50:41 +0200

    Consider the PYPICACHE_INDEX_PATH env var.
    Fixes #2. PYPICACHE_INDEX_PATH is considered in case there's no -p param provided. If neither -p param nor PYPICACHE_INDEX_PATH are available, then it just builds the cache in the current directory.

--- pypi_cache/cmd/update.py
+++ pypi_cache/cmd/update.py
@@ -1,5 +1,5 @@
 import argparse
-from pathlib import Path
+import os
 
 from pypi_cache.pypiCacheManager import LocalPyPIController
 
@@ -9,7 +9,7 @@ class Update:
     def init_subparser(parser: argparse.ArgumentParser):
         parser.add_argument("packageName", type=str, default="", help="Python package to add to the local repository.")
         parser.add_argument("-s", "--only-src", dest="onlySources", default=False, action="store_true", help="Download only the source files (.zip and .tar.gz). Disabled by default.")
-        parser.add_argument("-p", "--index-path", dest="pypiLocalPath", type=str, default=str(Path.home()) + "/.pypi-cache/", help="Local root path in which the package from the PyPI repository will be synchronized.")
+        parser.add_argument("-p", "--index-path", dest="pypiLocalPath", type=str, default=os.getenv('PYPICACHE_INDEX_PATH', default=".") + "/.pypi-cache/", help="Local root path in which the package from the PyPI repository will be synchronized.")
 
     @staticmethod
     def run(args: argparse.Namespace):
