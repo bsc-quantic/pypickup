dc8f119867170fe45c36a54710376f997506032e
Germán Navarro <ge.najim@gmail.com>
2022-09-13 16:30:55 +0200

    Allow multiple packages to be specified in the same command.
    Implements #4

--- pypi_cache/cmd/add.py
+++ pypi_cache/cmd/add.py
@@ -1,29 +1,38 @@
 import argparse
 import os
 
+from typing import List
+
 from pypi_cache.pypiCacheManager import LocalPyPIController
 
 
 class Add:
     @staticmethod
     def init_subparser(parser: argparse.ArgumentParser):
-        parser.add_argument("packageName", type=str, default="", help="Python package to add to the local repository.")
+        parser.add_argument("packageNameList", type=str, nargs='+', default="", help="Python packages list to add to the local repository.")
         parser.add_argument("-s", "--only-src", dest="onlySources", default=False, action="store_true", help="Download only the source files (.zip and .tar.gz). Disabled by default.")
         parser.add_argument("-p", "--index-path", dest="pypiLocalPath", type=str, default=os.getenv('PYPICACHE_INDEX_PATH', default="./.pypi-cache/"), help="Local root path in which the package from the PyPI repository will be downloaded.")
 
     @staticmethod
     def run(args: argparse.Namespace):
-        controllerInstance = LocalPyPIController()
+        listOfPackages: List[str] = args.packageNameList
+        for packageName in listOfPackages:
+            
+            args.packageName = packageName
+            print("Adding '" + packageName + "' to the local index:")
 
-        controllerInstance.parseScriptArguments(args)
-        
-        if controllerInstance.validPackageName():
-            controllerInstance.initLocalRepo()
+            controllerInstance = LocalPyPIController()
+            controllerInstance.parseScriptArguments(args)
+            
+            if controllerInstance.validPackageName():
+                controllerInstance.initLocalRepo()
 
-            needToAddPackage: bool = controllerInstance.canAddNewPackage()
-            if needToAddPackage:
-                controllerInstance.addPackage()
+                needToAddPackage: bool = controllerInstance.canAddNewPackage()
+                if needToAddPackage:
+                    controllerInstance.addPackage()
+                else:
+                    print("Package " + controllerInstance.packageName + " has been already added to the local repository. Try to run the 'update' command instead to synchronize changes with the remote PyPI.")
             else:
-                print("Package " + controllerInstance.packageName + " has been already added to the local repository. Try to run the 'update' command instead to synchronize changes with the remote PyPI.")
-        else:
-            print("Package " + controllerInstance.packageName + " does not exist in the remote repository (" + controllerInstance.remotePyPIRepository + ")")
+                print("Package " + controllerInstance.packageName + " does not exist in the remote repository (" + controllerInstance.remotePyPIRepository + ")")
+            
+            print()
--- pypi_cache/cmd/update.py
+++ pypi_cache/cmd/update.py
@@ -1,24 +1,33 @@
 import argparse
 import os
 
+from typing import List
+
 from pypi_cache.pypiCacheManager import LocalPyPIController
 
 
 class Update:
     @staticmethod
     def init_subparser(parser: argparse.ArgumentParser):
-        parser.add_argument("packageName", type=str, default="", help="Python package to add to the local repository.")
+        parser.add_argument("packageNameList", type=str, nargs='+', default="", help="Python packages list to add to the local repository.")
         parser.add_argument("-s", "--only-src", dest="onlySources", default=False, action="store_true", help="Download only the source files (.zip and .tar.gz). Disabled by default.")
         parser.add_argument("-p", "--index-path", dest="pypiLocalPath", type=str, default=os.getenv('PYPICACHE_INDEX_PATH', default="./.pypi-cache/"), help="Local root path in which the package from the PyPI repository will be synchronized.")
 
     @staticmethod
     def run(args: argparse.Namespace):
-        controllerInstance = LocalPyPIController()
+        listOfPackages: List[str] = args.packageNameList
+        for packageName in listOfPackages:
+            
+            args.packageName = packageName
+            print("Updating the local index for '" + packageName + "':")
+            
+            controllerInstance = LocalPyPIController()
+            controllerInstance.parseScriptArguments(args)
 
-        controllerInstance.parseScriptArguments(args)
-
-        canSynchronize: bool = controllerInstance.isAlreadyAdded()
-        if canSynchronize:
-            controllerInstance.synchronizeWithRemote()
-        else:
-            print("Package " + controllerInstance.packageName + " has not been added to the local repository yet. Run the 'add' command first.")
+            canSynchronize: bool = controllerInstance.isAlreadyAdded()
+            if canSynchronize:
+                controllerInstance.synchronizeWithRemote()
+            else:
+                print("Package " + controllerInstance.packageName + " has not been added to the local repository yet. Run the 'add' command first.")
+            
+            print()
