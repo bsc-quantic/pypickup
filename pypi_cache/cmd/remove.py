a370502cbc3f3be80e60c8b6bc1f2defbaa9333c
Germán Navarro <ge.najim@gmail.com>
2022-09-15 15:48:32 +0200

    Actually add the 'remove' command.

---
+++ pypi_cache/cmd/remove.py
@@ -0,0 +1,41 @@
+import argparse
+import os
+
+from typing import List
+
+from pypi_cache.pypiCacheManager import LocalPyPIController
+
+
+class Remove:
+    @staticmethod
+    def init_subparser(parser: argparse.ArgumentParser):
+        parser.add_argument("packageNameList", type=str, nargs="+", default="", help="Python packages list to be removed from the local repository.")
+        parser.add_argument("-p", "--index-path", dest="pypiLocalPath", type=str, default=os.getenv("PYPICACHE_INDEX_PATH", default="./.pypi-cache/"), help="Local root path in which the specified package is expected to be.")
+
+    @staticmethod
+    def __fillUpNonUsedArguments(args: argparse.Namespace) -> argparse.Namespace:
+        argsResult: argparse.Namespace = args
+        argsResult.onlySources = None
+        argsResult.includeDevs = None
+        argsResult.includeRCs = None
+        argsResult.includePlatformSpecific = None
+
+    @staticmethod
+    def run(args: argparse.Namespace):
+        Remove.__fillUpNonUsedArguments(args)
+
+        listOfPackages: List[str] = args.packageNameList
+        for packageName in listOfPackages:
+
+            args.packageName = packageName
+            print("Removing '" + packageName + "' from the local index:")
+
+            controllerInstance = LocalPyPIController()
+            controllerInstance.parseScriptArguments(args)
+
+            if controllerInstance.isAlreadyAdded():
+                controllerInstance.removePackage()
+            else:
+                print("Package " + controllerInstance.packageName + " has not been added to the local repository yet. Run the 'add' command first.")
+
+            print()
