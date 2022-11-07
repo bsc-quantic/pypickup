e1821880ccac9bf11f504c9a5f78ebb969779a7d
Germán Navarro <ge.najim@gmail.com>
2022-11-07 20:06:08 +0100

    Prepare a more sophisticated way to implement the wheel filters

--- pypickup/settings/wheelFilters.py
+++ pypickup/settings/wheelFilters.py
@@ -1,14 +1,37 @@
 filtersEnabled_wheels = "yes"   # yes/no
 
-inOrOut_wheels = "out"          # in/out
+inOrOut_wheels = "in"          # in/out
 
-# ToDo: use regexs. e.g.: "(python_tags>=1.2 | python_tags==1.01) & version>=18.0 & #^cp\d*d-\d*.whl$ | #^cp\d*d-manylinux14.whl$"
+# ToDo?: use regexs. e.g.: "(python_tags>=1.2 | python_tags==1.01) & version>=18.0 & #^cp\d*d-\d*.whl$ | #^cp\d*d-manylinux14.whl$"
 
+#### IN filters settings ####
+inFilters_wheels = {
+    "version": [],
+    "python_tags": [],
+    "abi_tags": [],
+    "platform_tags": ["~manylinux"]
+}
+in_ORorAnd = "and"
+in_ORorAndAttributes = {
+    "version": "or",
+    "python_tags": "or",
+    "abi_tags": "or",
+    "platform_tags": "and",
+}
+
+#### OUT filters settings ####
 outFilters_wheels = {
     "version": [],
     "python_tags": [],
     "abi_tags": [],
     "platform_tags": ["~macos"]
+}
+out_ORorAnd = "and"
+out_ORorAndAttributes = {
+    "version": "or",
+    "python_tags": "or",
+    "abi_tags": "or",
+    "platform_tags": "or",
 }
 
 from typing import Dict, List
@@ -19,9 +42,13 @@ class WheelsConfig:
     """
     A class to save the configuration for the pypickup commands.
 
-    The user can choose between filtering wheels in or out. If self.inOrOut == "in", the expected logic is that all packages will not be considere by
-    default, only if they match with the specified expressions. If self.inOurOut == "out", then all the packages will be included by default and only
-    those containing some of the specified expressions will be discarded.
+    The user can choose between filtering wheels in or out. If self.inOrOut == "in", the expected logic is that all packages will not be considered
+    by default, only if they match with the specified expressions. If self.inOurOut == "out", then all the packages will be included by default and 
+    only those containing some/all of the specified expressions will be discarded.
+
+    The _ORorAndAttributes modifiers states whether the values for each field should be concatenated with an 'or' or with an 'and' logical operator.
+    The _ORorAnd variable, in turn, states whether the previous logical results should be concatenated with an 'or' or with an 'and'. See the
+    examples section below for a specific example about this.
 
     One can exactly match a expression (default behaviour), or either using the "~" or ">", "<", ">=", "<=" modifiers:
         "~" can be used to indicate "it contains the following string", for every kind of filter.
@@ -43,19 +70,45 @@ class WheelsConfig:
 
     #### EXAMPLES ####
 
+    inOrOut_wheels = "in"
+
     inFilters_wheels = {
         "version": [],
         "python_tags": [">=3.5"],
         "abi_tags": ["~cp36", "~cp37"],
-        "platform_tags": ["~manylinux", "~win32", "~amd64"]
+        "platform_tags": ["~manylinux", "~x86_64"]
+    }
+    in_ORorAnd = "and"
+    in_ORorAndAttributes = {
+        "version": "or",
+        "python_tags": "or",
+        "abi_tags": "or",
+        "platform_tags": "and",
     }
 
     outFilters_wheels = {
         "version": ["~rc"],
         "python_tags": ["~pp", "~cp2", "<3.5"],
         "abi_tags": ["~mu"],
-        "platform_tags": ["~i686", "~win32"]
+        "platform_tags": ["~i686", "~win32", "~amd64"]
     }
+    out_ORorAnd = "and"
+    out_ORorAndAttributes = {
+        "version": "or",
+        "python_tags": "or",
+        "abi_tags": "or",
+        "platform_tags": "or",
+    }
+
+        * For this example, the logical expression that should be evaluated to true in order to include a certain 'wheel' file in the list of files
+          to be downloaded is:
+            if ((   wheel.python_tags >= 3.5 ) AND 
+                (   wheel.abi_tags.contains("cp36") OR wheel.abi_tags.contains("cp37") ) AND
+                (   wheel.platform_tags.contains("manylinux") AND wheel.platform_tags.contains("x86_64") ) ):
+                        includeWheel(wheel)
+
+        ** After this, if we would like to download too the files for AMD64 machines running Windows, we would specify in platform_tags those 2
+           filters, and perform an 'update' command on the same package.
 
     """
 
@@ -63,7 +116,6 @@ class WheelsConfig:
 
     def __init__(self):
         self._filtersEnabled: str = filtersEnabled_wheels
-
         self._inOrOut: str = inOrOut_wheels
 
         self._inFilters: Dict[str, List[str]] = None
@@ -71,6 +123,11 @@ class WheelsConfig:
 
         if "inFilters_wheels" in globals(): self._inFilters = inFilters_wheels
         if "outFilters_wheels" in globals(): self._outFilters = outFilters_wheels
+
+        self._in_ORorAnd: str = in_ORorAnd
+        self._in_ORorAndAttributes = in_ORorAndAttributes
+        self._out_ORorAnd: str = out_ORorAnd
+        self._out_ORorAndAttributes = out_ORorAndAttributes
 
     @property
     def filtersEnabled(self):
@@ -89,6 +146,22 @@ class WheelsConfig:
         return self._outFilters
 
     @property
+    def in_ORorAnd(self):
+        return self._in_ORorAnd
+
+    @property
+    def out_ORorAnd(self):
+        return self._out_ORorAnd
+
+    @property
+    def in_ORorAndAttributes(self):
+        return self._in_ORorAndAttributes
+
+    @property
+    def out_ORorAndAttributes(self):
+        return self._out_ORorAndAttributes
+
+    @property
     def incorrectInOrOutMessage(self):
         return self._incorrectInOrOutMessage
 
@@ -103,11 +176,31 @@ class WheelsConfig:
     def getField(self, fieldName: str) -> List[str]:
         if self.inOrOut == "in":
             if fieldName not in self.inFilters:
-                raise ValueError("Config::getField() - Field name not available in inFilters settings!")
+                raise ValueError("Config::getField() - Field '" + fieldName + "' name not available in inFilters settings!")
             return self.inFilters[fieldName]
         elif self.inOrOut == "out":
             if fieldName not in self.outFilters:
-                raise ValueError("Config::getField() - Field name not available in outFilters settings!")
+                raise ValueError("Config::getField() - Field '" + fieldName + "' name not available in outFilters settings!")
             return self.outFilters[fieldName]
+        else:
+            raise ValueError("Config::getField() - " + self._incorrectInOrOutMessage)
+
+    def getFilterConcatOperator(self) -> str:
+        if self.inOrOut == "in":
+            return self.in_ORorAnd
+        elif self.inOrOut == "out":
+            return self.out_ORorAnd
+        else:
+            raise ValueError("Config::getFilterConcatOperator() - " + self._incorrectInOrOutMessage)
+
+    def getFieldConcatOperator(self, fieldName: str) -> str:
+        if self.inOrOut == "in":
+            if fieldName not in self.in_ORorAndAttributes:
+                raise ValueError("Config::getField() - Field '" + fieldName + "' name not available in in_ORorAndAttributes settings!")
+            return self.in_ORorAndAttributes[fieldName]
+        elif self.inOrOut == "out":
+            if fieldName not in self.out_ORorAndAttributes:
+                raise ValueError("Config::getField() - Field '" + fieldName + "' name not available in out_ORorAndAttributes settings!")
+            return self.out_ORorAndAttributes[fieldName]
         else:
             raise ValueError("Config::getField() - " + self._incorrectInOrOutMessage)
